0c4ee2e5322967e1db016359c4486f5354f8872b
zivshamli <zivshamli100@gmail.com>
2024-10-01 23:01:44 +0300

    add SignalManager

--- app/src/main/java/com/example/finalprojecthobbiesconnect/SignInActivity.kt
+++ app/src/main/java/com/example/finalprojecthobbiesconnect/SignInActivity.kt
@@ -1,10 +1,7 @@
 package com.example.finalprojecthobbiesconnect
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
