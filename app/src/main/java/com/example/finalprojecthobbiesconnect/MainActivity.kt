088e1c6cccf365034408bd48cca9447a98f27a29
zivshamli <zivshamli100@gmail.com>
2024-10-01 23:00:27 +0300

    add SignalManager

--- app/src/main/java/com/example/finalprojecthobbiesconnect/MainActivity.kt
+++ app/src/main/java/com/example/finalprojecthobbiesconnect/MainActivity.kt
@@ -1,16 +1,75 @@
 package com.example.finalprojecthobbiesconnect
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.finalprojecthobbiesconnect.utilties.SignalManager
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var emailTextField: TextInputEditText
+    private lateinit var passwordTextField: TextInputEditText
+    private lateinit var loginBTN: MaterialButton
+    private lateinit var signUpBTN: MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+        findViews()
+        initViews()
+
+    }
+
+
+
+
+
+    private fun findViews() {
+        emailTextField = findViewById(R.id.email_text_field)
+        passwordTextField = findViewById(R.id.password_text_field)
+        loginBTN = findViewById(R.id.login_BTN)
+        signUpBTN = findViewById(R.id.sign_up_BTN)
+    }
+    private fun initViews() {
+        signUpBTN.setOnClickListener { changeToSignUp()}
+        loginBTN.setOnClickListener { login()}
+
+    }
+
+    private fun login() {
+        if (!checkTextFields()) {
+            return
+         }
+    }
+
+
+    private fun checkTextFields():Boolean {
+        if (emailTextField.text.toString().isEmpty() && passwordTextField.text.toString().isEmpty()) {
+            SignalManager.getInstance().vibrateAndToast("email and password missing!")
+            return false
+        }
+
+       else if (emailTextField.text.toString().isEmpty() ) {
+            SignalManager.getInstance().vibrateAndToast("email missing!")
+            return false
 
         }
+        else if (passwordTextField.text.toString().isEmpty()) {
+            SignalManager.getInstance().vibrateAndToast("password missing!")
+            return false
+        }
+        return true
     }
+
+    private fun changeToSignUp() {
+        val intent = Intent(this, SignInActivity::class.java)
+        val b = Bundle()
+        intent.putExtras(b)
+        startActivity(intent)
+        finish()
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/finalprojecthobbiesconnect/utilties/SignalManager.kt
@@ -0,0 +1,73 @@
+package com.example.finalprojecthobbiesconnect.utilties
+
+import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity.VIBRATOR_MANAGER_SERVICE
+import androidx.appcompat.app.AppCompatActivity.VIBRATOR_SERVICE
+import java.lang.ref.WeakReference
+
+class SignalManager private constructor(context: Context) {
+    private val contextRef = WeakReference(context)
+
+    fun vibrate() {
+        contextRef.get()?.let { context ->
+            val vibrator: Vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                val vibratorManager =
+                    context.getSystemService(VIBRATOR_MANAGER_SERVICE) as VibratorManager
+                vibratorManager.defaultVibrator
+            } else {
+                context.getSystemService(VIBRATOR_SERVICE) as Vibrator
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+
+                val oneShotVibrationEffect = VibrationEffect.createOneShot(
+                    500,
+                    VibrationEffect.DEFAULT_AMPLITUDE
+                )
+                vibrator.vibrate(oneShotVibrationEffect)
+            } else {
+                vibrator.vibrate(500)
+            }
+        }
+    }
+
+    fun toast(text: String) {
+        contextRef.get()?.let { context ->
+            Toast
+                .makeText(
+                    context,
+                    text,
+                    Toast.LENGTH_LONG
+                ).show()
+        }
+    }
+    fun vibrateAndToast(text: String) {
+        vibrate()
+        toast(text)
+    }
+
+
+    companion object {
+
+        @Volatile
+        private var instance: SignalManager? = null
+
+        fun init(context: Context): SignalManager {
+
+            return instance ?: synchronized(this) {
+                instance ?: SignalManager(context).also { instance = it }
+            }
+        }
+
+        fun getInstance(): SignalManager {
+            return instance
+                ?: throw IllegalStateException("SignalManager must be initialized by calling init(context) before use.")
+        }
+    }
+}
